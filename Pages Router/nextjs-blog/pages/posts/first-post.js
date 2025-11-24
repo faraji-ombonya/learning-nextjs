c2db230e9ebfccc7dcf3fc0fc9b96d850cda5522
Faraji Ombonya <farajiombonya@gmail.com>
2025-11-24 16:55:37 +0300

    chapter 12

--- Pages Router/nextjs-blog/pages/posts/first-post.js
+++ Pages Router/nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,17 @@
 import Link from "next/link";
+import Head from "next/head";
+
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
-      <h1>First Post</h1>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h2>
-        <Link href="/">Back Home</Link>
+        <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }

479aab62da519412b970fa3a81f5a1599de1ca86
Faraji Ombonya <farajiombonya@gmail.com>
2025-11-26 16:19:11 +0300

    chapter 14

--- Pages Router/nextjs-blog/pages/posts/first-post.js
+++ Pages Router/nextjs-blog/pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Head from "next/head";
+import Script from "next/script";
 
 import Layout from "../../components/layout";
 
@@ -9,6 +10,16 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
+
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(
+            "script loaded successfully, window.FB has been populated"
+          )
+        }
+      />
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
